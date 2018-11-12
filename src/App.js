e2e67c6b950944159ccdeeaeb0c3c0db9739108f
AbdeLaziz EL ARASSI <lailaelmanssour@gmail.com>
2018-11-12 18:24:38 +0100

    Created container for tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import IMG from "./wave.svg";
+import LOGO from "./logo.png";
 
 class App extends Component {
   render() {
@@ -9,6 +10,13 @@ class App extends Component {
         <div className="container">
           <div className="blue-container" />
           <img className="wave" src={IMG} />
+        </div>
+        <div className="container2">
+          <img className="logo" src={LOGO} />
+          <div className="tasks-container">
+            <p>heloo</p>
+            <p>heloo</p>
+          </div>
         </div>
       </>
     );
